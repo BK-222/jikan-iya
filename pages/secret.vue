338026179d899f7641a252bc8949040de0155d07
BK-222 <letsgetjscoding@gmail.com>
2025-01-01 23:59:17 +0000

    Added a prelim. secret page.

---
+++ pages/secret.vue
@@ -0,0 +1,19 @@
+<script setup>
+import useAuthStore from '@/stores/auth';
+
+const store = useAuthStore();
+const router = useRouter();
+
+const logout = function() {
+  store.logout();
+  router.replace('/login');
+}
+</script>
+
+<template>
+  <div>
+    <h1>Secret Page</h1>
+    <p>Welcome to the secret page, only accessible to authenticated users!</p>
+    <base-button @click="logout">Logout</base-button>
+  </div>
+</template>
