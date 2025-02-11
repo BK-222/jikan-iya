bc474443a230dc336a61093293944dd73552610d
BK-222 <letsgetjscoding@gmail.com>
2025-02-11 23:58:41 +0000

    Button-links now transparent on index.vue.

--- pages/index.vue
+++ pages/index.vue
@@ -17,11 +17,18 @@ onMounted(() => {
 <template>
   <div class="flex flex-col items-center justify-center min-h-screen p-4 relative">
     <div class="flex gap-4 mb-4 md:flex-col md:absolute md:top-6 md:left-4 md:mb-0 md:gap-2 md:w-32">
-
-      <NuxtLink to="/mainstream"><BaseButton class="w-full px-2 py-1 rounded-none bg-gray-400">mainstream</BaseButton></NuxtLink>
-      <NuxtLink to="/iyashikei"><BaseButton class="w-full px-2 py-1 rounded-none bg-gray-400">iyashikei</BaseButton></NuxtLink>
-      <NuxtLink v-if="!isLoggedIn" to="/auth"><BaseButton class="w-full px-2 py-1 rounded-none">authenticate</BaseButton></NuxtLink>
-      <NuxtLink v-else :to="`/profile/${authStore.userId}`"><BaseButton class="w-full rounded-none">profile</BaseButton></NuxtLink>
+      <NuxtLink to="/mainstream">
+        <BaseButton class="w-full !py-1 bg-transparent border border-2 border-teal-500 text-teal-500 hover:bg-teal-500 hover:text-white">mainstream</BaseButton>
+      </NuxtLink>
+      <NuxtLink to="/iyashikei">
+        <BaseButton class="w-full !py-1 bg-transparent border-2 border-teal-500 text-teal-500 hover:bg-teal-500 hover:text-white">iyashikei</BaseButton>
+      </NuxtLink>
+      <NuxtLink v-if="!isLoggedIn" to="/auth">
+        <BaseButton class="w-full !py-1 bg-transparent border-2 border-teal-500 text-teal-500 hover:bg-teal-500 hover:text-white">authenticate</BaseButton>
+      </NuxtLink>
+      <NuxtLink v-else :to="`/profile/${authStore.userId}`">
+        <BaseButton class="w-full !py-1 bg-transparent border-2 border-teal-500 text-teal-500 hover:bg-teal-500 hover:text-white">profile</BaseButton>
+      </NuxtLink>
     </div>
 
     <div class="text-center flex-1">
