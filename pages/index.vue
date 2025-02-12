586d700fa977946f3ac0d03dafcb50224eb72bda
BK-222 <letsgetjscoding@gmail.com>
2025-02-12 00:06:16 +0000

    Gave links original button colors but with border and transparent on hover.

--- pages/index.vue
+++ pages/index.vue
@@ -18,16 +18,16 @@ onMounted(() => {
   <div class="flex flex-col items-center justify-center min-h-screen p-4 relative">
     <div class="flex gap-4 mb-4 md:flex-col md:absolute md:top-6 md:left-4 md:mb-0 md:gap-2 md:w-32">
       <NuxtLink to="/mainstream">
-        <BaseButton class="w-full !py-1 bg-transparent border border-2 border-teal-500 text-teal-500 hover:bg-teal-500 hover:text-white">mainstream</BaseButton>
+        <BaseButton class="w-full !py-1 hover:bg-transparent border border-teal-600 hover:text-teal-500">mainstream</BaseButton>
       </NuxtLink>
       <NuxtLink to="/iyashikei">
-        <BaseButton class="w-full !py-1 bg-transparent border-2 border-teal-500 text-teal-500 hover:bg-teal-500 hover:text-white">iyashikei</BaseButton>
+        <BaseButton class="w-full !py-1 hover:bg-transparent border border-teal-600 hover:text-teal-500">iyashikei</BaseButton>
       </NuxtLink>
       <NuxtLink v-if="!isLoggedIn" to="/auth">
-        <BaseButton class="w-full !py-1 bg-transparent border-2 border-teal-500 text-teal-500 hover:bg-teal-500 hover:text-white">authenticate</BaseButton>
+        <BaseButton class="w-full !py-1 hover:bg-transparent border border-teal-600 hover:text-teal-500">authenticate</BaseButton>
       </NuxtLink>
       <NuxtLink v-else :to="`/profile/${authStore.userId}`">
-        <BaseButton class="w-full !py-1 bg-transparent border-2 border-teal-500 text-teal-500 hover:bg-teal-500 hover:text-white">profile</BaseButton>
+        <BaseButton class="w-full !py-1 hover:bg-transparent border border-teal-600 hover:text-teal-500">profile</BaseButton>
       </NuxtLink>
     </div>
 
